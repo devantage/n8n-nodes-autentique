6d3608203cc65eb09f1839a5c92cdb20b422f3ff
Guilherme Nogueira <guilherme.lopesn@gmail.com>
2025-10-03 12:17:23 -0300

    feat: add list documents operation

--- src/resources/document/document.resource.ts
+++ src/resources/document/document.resource.ts
@@ -1,8 +1,9 @@
 import { Resource } from '../models';
 import { CreateOperation } from './create';
+import { ListOperation } from './list';
 
 export class DocumentResource extends Resource {
   public constructor() {
-    super('document', 'Document', CreateOperation);
+    super('document', 'Document', CreateOperation, ListOperation);
   }
 }
