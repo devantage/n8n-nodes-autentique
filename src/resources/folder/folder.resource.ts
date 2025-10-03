21fbf19af5a964a56055c7b02d9fc3a9ada9548f
Guilherme Nogueira <guilherme.lopesn@gmail.com>
2025-10-03 12:45:28 -0300

    feat: add list folders operation

--- src/resources/folder/folder.resource.ts
+++ src/resources/folder/folder.resource.ts
@@ -1,8 +1,9 @@
 import { Resource } from '../models';
 import { CreateOperation } from './create';
+import { ListOperation } from './list';
 
 export class FolderResource extends Resource {
   public constructor() {
-    super('folder', 'Folder', CreateOperation);
+    super('folder', 'Folder', CreateOperation, ListOperation);
   }
 }
